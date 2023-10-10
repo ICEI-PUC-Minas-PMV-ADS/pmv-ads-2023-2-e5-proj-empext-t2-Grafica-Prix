79f0cdf5a24c0c88721541e613b4452c1baca2e5
Arthur <arthur.rocha@jobconvo.com>
2023-10-09 21:14:16 -0300

    Arthur - Atualização back

---
+++ src/Server/API-Grafica-Prix/API-Grafica-Prix/Migrations/20231009234953_AddCategoriesAndProducts.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Grafica_Prix.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoriesAndProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
