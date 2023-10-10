5f9cb2ef4a3605661c339d53f7761bb320d1f576
Cerian-ui <barbara.augusta@hotmail.com>
2023-10-09 21:08:46 -0300

    Barbara - Criação dropdown

--- src/web/src/components/common/container/styles.js
+++ src/web/src/components/common/container/styles.js
@@ -1,13 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const ContainerStyles = styled.div`
-  max-width: ${(props) => props.maxWidth || "1200px"};
-  padding: ${(props) => props.padding || "20px"};
-  margin: ${(props) => props.margin || "0 auto"};
+  max-width: ${props => props.maxWidth || '1200px'};
+  padding: ${props => props.padding || '20px'};
+  margin: ${props => props.margin || '0 auto'};
   display: flex;
-  flex-direction: ${(props) => props.direction || "column"};
-  justify-content: ${(props) => props.justifyContent || "flex-start"};
-  align-items: ${(props) => props.alignItems || "flex-start"};
-  height: ${(props) => props.height};
-  gap: ${(props) => props.gap || "10px"};
-`;
+  flex-direction: ${props => props.direction || 'column'};
+  justify-content: ${props => props.justifyContent || 'flex-start'};
+  align-items: ${props => props.alignItems || 'flex-start'};
+  height: ${props => props.height};
+  gap: ${props => props.gap || '10px'};
+  position: relative;
+`
