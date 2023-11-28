d6083353732c2e307d75c7b5c9bf383e210284b0
Arthur <arthur.rocha@jobconvo.com>
2023-11-28 14:46:02 -0300

    Correção string de conexão com a api

--- src/web/src/services/http.js
+++ src/web/src/services/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://localhost:7187/",
+  baseURL: "http://prixsystem-001-site1.anytempurl.com/",
 });
 
 export default http;
